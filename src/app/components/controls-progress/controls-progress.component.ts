3908fca0e4273e93ed8658078e11218e8bb9d720
Alvarez Olavarría Nicolás <nalvarezex@bice.cl>
2022-04-20 10:13:41 -0400

    progress bar terminado

--- src/app/components/controls-progress/controls-progress.component.ts
+++ src/app/components/controls-progress/controls-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-controls-progress',
@@ -8,25 +8,25 @@ import { Component } from '@angular/core';
 })
 export class ControlsProgressComponent {
 
-  progreso: number =  25;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`;
-  }
+  @Input() progreso: number =  25;
+  @Output() emiteValor: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
     
     if (this.progreso >= 100 && valor >= 0) {
+      this.emiteValor.emit(100);
       this.progreso = 100;
       return;
     }
 
     if (this.progreso <= 0 && valor < 0) {
+      this.emiteValor.emit(0);
       this.progreso = 0;
       return;
     }
 
     this.progreso =  this.progreso + valor;
+    this.emiteValor.emit(this.progreso);
   }
 
 }
